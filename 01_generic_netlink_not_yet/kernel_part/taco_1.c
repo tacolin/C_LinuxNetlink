be2c9caea63b3d39d3821c6bd5180d48ea335390
taco <taco@taco-vm.(none)>
2014-03-03 19:34:38 +0800

    generic netlink prototype ok

--- 01_generic_netlink_not_yet/kernel_part/taco_1.c
+++ 01_generic_netlink_not_yet/kernel_part/taco_1.c
@@ -1,68 +1,175 @@
-#include <linux/types.h>
 #include <linux/kernel.h>
-#include <linux/netlink.h>
-#include <net/sock.h>
-#include <linux/sched.h>
 #include <linux/module.h>
-#include <linux/init.h>
+#include <net/genetlink.h>
 
 MODULE_AUTHOR("tacolin");
-MODULE_DESCRIPTION("NETLINK KERNEL TEST MODULE");
+MODULE_DESCRIPTION("GE-NETLINK KERNEL TEST MODULE");
 MODULE_LICENSE("GPL");
 
-#define NETLINK_TEST 17
-#define MAX_PAYLOAD  512
+typedef unsigned int uint32;
 
-static struct sock* g_pSocket = NULL;
+static int _doEcho(struct sk_buff *pSkb, struct genl_info *pInfo);
 
-static void recv_msg_from_user(struct sk_buff *pSkb, char* pRecvBuf, int *pPid)
+typedef enum
 {
-    struct nlmsghdr *pNlhdr = (struct nlmsghdr*)(pSkb->data);
+    TACO_ATTRIBUTE_1_INTEGER,
+    TACO_ATTRIBUTE_2_STRING,
 
-    memcpy(pRecvBuf, nlmsg_data(pNlhdr), strlen( (char*)nlmsg_data(pNlhdr) ) );
-
-    *pPid = pNlhdr->nlmsg_pid;
+    TACO_ATTRIBUTE_MAX
 }
+tTacoAttributes;
 
-static void send_msg_to_user(struct sock* pSocket, int pid, char* pSendBuf)
+static struct genl_family g_kernelFamily = 
 {
-    int msgSize = 0;
-    int sendResult = -1;
-    struct sk_buff *pSkb;
-    struct nlmsghdr *pNlhdr;
+    .id = GENL_ID_GENERATE,
+    .hdrsize = 0,
+    .name = "TACO_KERNEL",
+    .version = 1,
+    .maxattr = TACO_ATTRIBUTE_MAX-1,
+};
 
-    msgSize = strlen(pSendBuf);
-    pSkb = nlmsg_new( msgSize, 0 );
-    pNlhdr = nlmsg_put(pSkb, 0, 0, NLMSG_DONE, msgSize, 0);
+static struct nla_policy g_kernelAttributeCheckPolicy[TACO_ATTRIBUTE_MAX] = 
+{
+    [TACO_ATTRIBUTE_1_INTEGER] = { .type = NLA_U32 },
+    [TACO_ATTRIBUTE_2_STRING] = { .type = NLA_NUL_STRING },
+};
 
-    sprintf( nlmsg_data(pNlhdr), pSendBuf, msgSize );
-    NETLINK_CB(pSkb).dst_group = 0;
+typedef enum
+{
+    TACO_OPERATION_UNSPEC,
+    TACO_OPERATION_ECHO,
 
-    sendResult =  nlmsg_unicast(pSocket, pSkb, pid);
-    if ( sendResult < 0 )
+    TACO_OPERATION_MAX
+}
+tTacoOperations;
+
+static struct genl_ops g_kernelOperationEcho = 
+{
+    .cmd = TACO_OPERATION_ECHO,
+    .flags = 0,
+    .policy = g_kernelAttributeCheckPolicy,
+    .doit = _doEcho,
+};
+
+
+static int _parseMsgFromUseSpace(struct genl_info *pInfo)
+{
+    struct nlattr *pAttr1 = NULL;
+    uint32 data1;
+    struct nlattr *pAttr2 = NULL;
+    char* pData2;
+
+    pAttr1 = pInfo->attrs[TACO_ATTRIBUTE_1_INTEGER];
+    if ( pAttr1 != NULL)
     {
-        printk("[KERNEL-PART] unicast a message to user failed\n");
+        data1 = *(uint32*)nla_data(pAttr1);
+        printk("[KERNEL-PART] data1 = %d\n", data1);
     }
+
+    pAttr2 = pInfo->attrs[TACO_ATTRIBUTE_2_STRING];
+    if ( pAttr2 != NULL )
+    {
+        pData2 = (char*)nla_data(pAttr2);
+        printk("[KERNEL-PART] data2 = %s\n", pData2);
+    }
+
+    return 0;
 }
 
-static void process_user_msg(struct sk_buff *pSkb) 
+static int _sendMsgToUserSpace(struct genl_info *pInfo)
 {
-    int pid = -1;
-    char pMsgBuf[MAX_PAYLOAD];
+    struct sk_buff *pSendbackSkb = NULL;
+    void *pMsgHead = NULL;
+    int nlaputResult;
+    int sendbackResult;
 
-    memset(pMsgBuf, 0, MAX_PAYLOAD);
-    recv_msg_from_user(pSkb, pMsgBuf, &pid);
+    pSendbackSkb = genlmsg_new(NLMSG_GOODSIZE, GFP_KERNEL);
+    if ( pSendbackSkb == NULL ) 
+    {
+        printk("[KERNEL-ERROR] sendback skb is null\n");
+        return -1;
+    }
 
-    printk("[KERNEL-PART] received pid=%d's message : %s\n", pid, pMsgBuf);
+    pMsgHead = genlmsg_put(pSendbackSkb, 0, pInfo->snd_seq+1, &g_kernelFamily, 0, TACO_OPERATION_ECHO);
+    if ( pMsgHead == NULL )
+    {
+        printk("[KERNEL-ERROR] genl_put failed\n");
+        return -1;
+    }
 
-    memset(pMsgBuf, 0, MAX_PAYLOAD);
-    sprintf(pMsgBuf, "Hello from Kernel");
-    send_msg_to_user(g_pSocket, pid, pMsgBuf);
+    nlaputResult = nla_put_u32(pSendbackSkb, TACO_ATTRIBUTE_1_INTEGER, 9999);
+    if ( nlaputResult != 0 )
+    {
+        printk("[KERNEL-ERROR] nla_put_u32 failed\n");
+        return -1;
+    }
+
+    nlaputResult = nla_put_string(pSendbackSkb, TACO_ATTRIBUTE_2_STRING, "Hello From Kernel");
+    if ( nlaputResult != 0 )
+    {
+        printk("[KERNEL-ERROR] nla_put_string failed\n");
+        return -1;
+    }
+
+    genlmsg_end(pSendbackSkb, pMsgHead);
+
+    sendbackResult = genlmsg_unicast(&init_net, pSendbackSkb, pInfo->snd_pid);
+    if ( sendbackResult != 0 )
+    {
+        printk("[KERNEL-ERROR] genl_unicast failed\n");
+        return -1;   
+    }
+
+    return 0;
 }
+
+static int _doEcho(struct sk_buff *pSkb, struct genl_info *pInfo)
+{
+    if ( pSkb == NULL )
+    {
+        printk("[KERNEL-ERROR] taco_doEcho : skb is null\n");
+        return -1;
+    }
+
+    if ( pInfo == NULL )
+    {
+        printk("[KERNEL-ERROR] taco_doEcho : info is null\n");
+        return -1;
+    }
+
+    if ( 0 != _parseMsgFromUseSpace(pInfo) )
+    {
+        printk("[KERNEL-PART] parse msg from user space failed\n");
+        return -1;
+    }
+
+    if ( 0 != _sendMsgToUserSpace(pInfo) )
+    {
+        printk("[KERNEL-PART] send msg to user space failed\n");
+        return -1;
+    }   
+
+    return 0;
+}
+
 
 static int __init kernel_module_init(void)
 {
-    g_pSocket = netlink_kernel_create(&init_net, NETLINK_TEST, 0, process_user_msg, NULL, THIS_MODULE);
+    int registerResult;
+
+    registerResult = genl_register_family(&g_kernelFamily);
+    if ( registerResult != 0 )
+    {
+        printk("[KERNEL-ERROR] Register Family failed\n");
+        return -1;
+    }
+
+    registerResult = genl_register_ops(&g_kernelFamily, &g_kernelOperationEcho);
+    if ( registerResult != 0 )
+    {   
+        printk("[KERNEL-ERROR] Register Operations failed\n");
+        return -1;
+    }
 
     printk("[KERNEL-PART] Module Inserted\n");
 
@@ -71,7 +178,19 @@ static int __init kernel_module_init(void)
 
 static void __exit kernel_module_exit(void)
 {
-    netlink_kernel_release(g_pSocket);
+    int unregisterResult;
+
+    unregisterResult = genl_unregister_ops(&g_kernelFamily, &g_kernelOperationEcho);
+    if ( unregisterResult != 0 )
+    {
+        printk("[KERNEL-ERROR] Unregister Operation failed\n");
+    }
+
+    unregisterResult = genl_unregister_family(&g_kernelFamily);
+    if ( unregisterResult != 0 )
+    {
+        printk("[KERNEL-ERROR] Unregister Family failed\n");
+    }
 
     printk("[KERNEL-PART] Module removed\n");
 
--- 01_generic_netlink_not_yet/user_part/main.c
+++ 01_generic_netlink_not_yet/user_part/main.c
@@ -4,14 +4,42 @@
 #include <sys/socket.h>
 #include <linux/genetlink.h>
 
-#define NETLINK_TEST 17
-#define MAX_PAYLOAD  512
+#define GENLMSG_DATA(glh) ((void *)(NLMSG_DATA(glh) + GENL_HDRLEN))
+#define GENLMSG_PAYLOAD(glh) (NLMSG_PAYLOAD(glh, 0) - GENL_HDRLEN)
+#define NLA_DATA(na) ((void *)((char*)(na) + NLA_HDRLEN))
 
-int main(int argc, char* argv[])
+typedef enum
+{
+    TACO_OPERATION_UNSPEC,
+    TACO_OPERATION_ECHO,
+
+    TACO_OPERATION_MAX
+}
+tTacoOperations;
+
+typedef enum
+{
+    TACO_ATTRIBUTE_1_INTEGER,
+    TACO_ATTRIBUTE_2_STRING,
+
+    TACO_ATTRIBUTE_MAX
+}
+tTacoAttributes;
+
+typedef struct _tSelfDefinedMsg
+{
+    struct nlmsghdr nlhdr;
+    struct genlmsghdr genlhdr;
+    char buf[256];
+}
+tSelfDefinedMsg;
+
+typedef unsigned int uint32;
+
+static int _createGenericNetlinkSocket(void)
 {
     int socketFd;
-    socketFd = socket(AF_NETLINK, SOCK_RAW, NETLINK_TEST);
-
+    socketFd = socket(AF_NETLINK, SOCK_RAW, NETLINK_GENERIC);
 
     struct sockaddr_nl srcAddr;
     memset( &srcAddr, 0, sizeof(srcAddr) );
@@ -20,44 +48,177 @@ int main(int argc, char* argv[])
     srcAddr.nl_groups = 0;
     bind( socketFd, (struct sockaddr*)&srcAddr, sizeof(srcAddr) );
 
+    return socketFd;
+}
+
+static int _sendQueryRequest(int socketFd, char* pFamilyName)
+{
+    tSelfDefinedMsg request;
+    struct nlattr *pAttr = NULL;
+
+    memset(&request, 0, sizeof(tSelfDefinedMsg) );
+    
+    request.nlhdr.nlmsg_type = GENL_ID_CTRL;
+    request.nlhdr.nlmsg_flags = NLM_F_REQUEST;
+    request.nlhdr.nlmsg_seq = 0;
+    request.nlhdr.nlmsg_pid = getpid();
+    request.nlhdr.nlmsg_len = NLMSG_LENGTH(GENL_HDRLEN);
+    
+    request.genlhdr.cmd = CTRL_CMD_GETFAMILY;
+    request.genlhdr.version = 1;
+
+    pAttr = (struct nlattr*)GENLMSG_DATA(&request);
+    pAttr->nla_type = CTRL_ATTR_FAMILY_NAME;
+    pAttr->nla_len = strlen( pFamilyName ) + 1 + NLA_HDRLEN;
+    strcpy( NLA_DATA(pAttr), pFamilyName );
+
+    request.nlhdr.nlmsg_len += NLMSG_ALIGN( pAttr->nla_len );
+
+    struct sockaddr_nl srcAddr;
+    memset( &srcAddr, 0, sizeof(srcAddr) );
+    srcAddr.nl_family = AF_NETLINK;
+
+    if ( 0 >= sendto(socketFd, &request, request.nlhdr.nlmsg_len, 0, (struct sockaddr*)&srcAddr, sizeof(srcAddr)) )
+    {
+        printf("[USER-ERROR] query family id : sendto kerenl failed\n");
+        return -1;
+    }
+
+    return 0;
+}
+
+static int _recvAndParseQueryResponse(int socketFd)
+{
+    tSelfDefinedMsg response;
+    struct nlattr *pRspAttr = NULL;
+    int recvLen;
+    int familyId = -1;
+
+    recvLen = recv( socketFd, &response, sizeof(response), 0 );
+    if ( recvLen <= 0 ) 
+    {
+        printf("[USER-ERROR] query family id : recv from kernel failed\n");
+        return -1;
+    }
+
+    if ( !NLMSG_OK( &(response.nlhdr), recvLen  ) )
+    {
+        printf("[USER-ERROR] query family id : invalid reponse\n");
+        return -1;
+    }
+    
+    if ( response.nlhdr.nlmsg_type == NLMSG_ERROR )
+    {
+        printf("[USER-ERROR] query family id : response type is error\n");
+        return -1;
+    }
+
+    pRspAttr = (struct nlattr*)GENLMSG_DATA(&response);
+    pRspAttr = (struct nlattr*)( (char*)(pRspAttr) + NLA_ALIGN(pRspAttr->nla_len) );
+    if ( pRspAttr->nla_type == CTRL_ATTR_FAMILY_ID ) 
+    {
+        familyId = *(__u16 *)NLA_DATA(pRspAttr);
+    }
+
+    return familyId;
+}
+
+static int _queryKernelFamilyId(int socketFd, char* pFamilyName)
+{
+    int familyId;
+
+    _sendQueryRequest(socketFd, pFamilyName);
+
+    familyId = _recvAndParseQueryResponse(socketFd);
+    return familyId;    
+}
+
+static int _sendDataToTacoKernel(int socketFd, int familyId)
+{
+    tSelfDefinedMsg req;
+    memset(&req, 0, sizeof(tSelfDefinedMsg));
+    req.nlhdr.nlmsg_len = NLMSG_LENGTH(GENL_HDRLEN);
+    req.nlhdr.nlmsg_type = familyId;
+    req.nlhdr.nlmsg_flags = NLM_F_REQUEST;
+    req.nlhdr.nlmsg_seq = 100;
+    req.nlhdr.nlmsg_pid = getpid();
+
+    req.genlhdr.cmd = TACO_OPERATION_ECHO;
+
+    struct nlattr* pAttr;
+    pAttr = (struct nlattr*)GENLMSG_DATA(&req);
+    pAttr->nla_type = TACO_ATTRIBUTE_1_INTEGER;
+    pAttr->nla_len = sizeof(uint32) + NLA_HDRLEN;
+    *(uint32*)NLA_DATA(pAttr) = 123;
+
+    req.nlhdr.nlmsg_len += NLMSG_ALIGN(pAttr->nla_len);
+
+    pAttr = (struct nlattr*)( (void*)pAttr + NLMSG_ALIGN(pAttr->nla_len) );
+    pAttr->nla_type = TACO_ATTRIBUTE_2_STRING;
+    pAttr->nla_len = strlen("Hello from User") + 1 + NLA_HDRLEN;
+    strcpy( NLA_DATA(pAttr), "Hello from User");
+
+    req.nlhdr.nlmsg_len += NLMSG_ALIGN(pAttr->nla_len);
 
     struct sockaddr_nl dstAddr;
     memset( &dstAddr, 0, sizeof(dstAddr) );
     dstAddr.nl_family = AF_NETLINK;
-    dstAddr.nl_pid    = 0;
-    dstAddr.nl_groups = 0;
+
+    sendto( socketFd, &req, req.nlhdr.nlmsg_len, 0, (struct sockaddr*)&dstAddr, sizeof(dstAddr) );
+
+    return 0;
+
+}
+
+static int _recvAndParseDataFromTacoKernel(int socketFd)
+{
+    tSelfDefinedMsg response;
+    int recvLen;
+
+    recvLen = recv(socketFd, &response, sizeof(response), 0);
+    if ( !NLMSG_OK( &(response.nlhdr), recvLen ) )
+    {
+        printf("[USER-ERROR] recv invalid data from kernel\n");
+        return -1;
+    }
+
+    if ( response.nlhdr.nlmsg_type == NLMSG_ERROR )
+    {
+        printf("[USER-ERROR] received nlmsg type is error\n");
+        return -1;
+    }
+
+    struct nlattr* pAttr;
+    pAttr = (struct nlattr*)GENLMSG_DATA(&response);
+    int data1 = *(uint32*)NLA_DATA(pAttr);
+    printf("[USER-PART] data1 = %d\n", data1);
+
+    pAttr = (struct nlattr*)( (void*)pAttr + pAttr->nla_len );
+    char *pData2 = (char*)NLA_DATA(pAttr);
+    printf("[USER-PART] data2 = %s\n", pData2);
+
+    return 0;
+}
 
 
-    struct nlmsghdr *pNlhdr;
-    pNlhdr = (struct nlmsghdr*)malloc( NLMSG_SPACE(MAX_PAYLOAD) );
-    memset( pNlhdr, 0, NLMSG_SPACE(MAX_PAYLOAD) );
-    pNlhdr->nlmsg_len   = NLMSG_SPACE(MAX_PAYLOAD);
-    pNlhdr->nlmsg_pid   = getpid();
-    pNlhdr->nlmsg_flags = 0;    
-    sprintf( NLMSG_DATA(pNlhdr), "Hello from User" );
+int main(int argc, char* argv[])
+{
+    int socketFd = _createGenericNetlinkSocket();
+    if ( socketFd < 0 )   
+    {
+        printf("[USER-ERROR] create generic netlink failed\n");
+        return -1;
+    }   
+    
+    int familyId = _queryKernelFamilyId(socketFd, "TACO_KERNEL");
+    if ( familyId < 0 )
+    {
+        printf("[USER-ERROR] query kernel family id failed\n");
+        return -1;
+    }    
 
-
-    struct iovec iov;
-    memset( &iov, 0, sizeof(struct iovec) );
-    iov.iov_base = (void*)pNlhdr;
-    iov.iov_len  = pNlhdr->nlmsg_len;
-
-
-    struct msghdr msg;
-    memset( &msg, 0, sizeof(struct msghdr) );
-    msg.msg_name    = (void*)&dstAddr;
-    msg.msg_namelen = sizeof(dstAddr);
-    msg.msg_iov = &iov;
-    msg.msg_iovlen = 1;    
-
-
-    sendmsg( socketFd, &msg, 0 );
-
-
-    memset( pNlhdr, 0, NLMSG_SPACE(MAX_PAYLOAD) );
-    recvmsg( socketFd, &msg, 0 );
-    printf("[USER-PART] Receive message from kernel : %s\n", (char*)NLMSG_DATA(pNlhdr) );
-
+    _sendDataToTacoKernel(socketFd, familyId);
+    _recvAndParseDataFromTacoKernel(socketFd);
 
     close(socketFd);
 
