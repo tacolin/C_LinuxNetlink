80b1702e368812ff9d33411d5a1835c28d805838
tacolin <potegrant@yahoo.com.tw>
2015-01-11 23:08:29 +0800

    修改kernel part 變數及function name

--- 02_netlink_performance/kernel_part/kmain.c
+++ 02_netlink_performance/kernel_part/kmain.c
@@ -4,93 +4,92 @@ MODULE_AUTHOR("tacolin");
 MODULE_DESCRIPTION("NETLINK KERNEL TEST MODULE");
 MODULE_LICENSE("GPL");
 
-static struct sock* g_pSocket = NULL;
+static struct sock* _socket = NULL;
 
 static unsigned char _rxBuffer[MAX_PAYLOAD] = {};
 
-static void recv_msg_from_user(struct sk_buff *pSkb, char* pRecvBuf, int *pPid)
+static void recv_msg_from_user(struct sk_buff *skb, char* recvBuf, int *pid)
 {
-    struct nlmsghdr *pNlhdr = (struct nlmsghdr*)(pSkb->data);
+    struct nlmsghdr *nlhdr = (struct nlmsghdr*)(skb->data);
 
-    memcpy(pRecvBuf, nlmsg_data(pNlhdr), nlmsg_len(pNlhdr) );
+    memcpy(recvBuf, nlmsg_data(nlhdr), nlmsg_len(nlhdr) );
 
-    *pPid = pNlhdr->nlmsg_pid;
+    *pid = nlhdr->nlmsg_pid;
 }
 
-static void send_msg_to_user(struct sock* pSocket, int pid, char* pSendBuf)
+static void send_msg_to_user(struct sock* socket, int pid, char* sendBuf)
 {
     int msgSize = 0;
     int sendResult = -1;
-    struct sk_buff *pSkb;
-    struct nlmsghdr *pNlhdr;
+    struct sk_buff *skb;
+    struct nlmsghdr *nlhdr;
 
-    msgSize = strlen(pSendBuf);
-    pSkb = nlmsg_new( msgSize, 0 );
-    pNlhdr = nlmsg_put(pSkb, 0, 0, NLMSG_DONE, msgSize, 0);
+    msgSize = strlen(sendBuf);
+    skb = nlmsg_new( msgSize, 0 );
+    nlhdr = nlmsg_put(skb, 0, 0, NLMSG_DONE, msgSize, 0);
 
-    sprintf( nlmsg_data(pNlhdr), pSendBuf, msgSize );
-    NETLINK_CB(pSkb).dst_group = 0;
+    sprintf( nlmsg_data(nlhdr), sendBuf, msgSize );
+    NETLINK_CB(skb).dst_group = 0;
 
-    sendResult =  nlmsg_unicast(pSocket, pSkb, pid);
+    sendResult =  nlmsg_unicast(socket, skb, pid);
     CHECK_IF(sendResult < 0, return, "unicast a message to user failed");
 }
 
-static void process_user_msg(struct sk_buff *pSkb)
+static void process_user_msg(struct sk_buff *skb)
 {
     int pid = -1;
 
     memset(_rxBuffer, 0, MAX_PAYLOAD);
-    recv_msg_from_user(pSkb, _rxBuffer, &pid);
+    recv_msg_from_user(skb, _rxBuffer, &pid);
 
     dprint("received pid=%d's message : %s", pid, _rxBuffer);
 
     memset(_rxBuffer, 0, MAX_PAYLOAD);
     sprintf(_rxBuffer, "Hello from Kernel");
-    send_msg_to_user(g_pSocket, pid, _rxBuffer);
+    send_msg_to_user(_socket, pid, _rxBuffer);
 }
 
-static struct sock* netlink_create_wrapper(struct net *pNet, int unit, unsigned int groups, void (*input)(struct sk_buff* skb), struct mutex *pCb_mutex, struct module *pModule)
+static struct sock* netlink_create_wrapper(struct net *net, int unit, unsigned int groups, void (*input)(struct sk_buff* skb), struct mutex *cb_mutext, struct module *module)
 {
 #if LINUX_VERSION_CODE < KERNEL_VERSION(3, 5, 0)
     derror("do not support linux kernel version less than 3.5");
     return NULL;
 #elif LINUX_VERSION_CODE < KERNEL_VERSION(3, 6, 0)
-    return netlink_kernel_create(pNet, unit, groups, input, pCb_mutex, pModule);
+    return netlink_kernel_create(net, unit, groups, input, cb_mutext, module);
 #elif LINUX_VERSION_CODE < KERNEL_VERSION(3, 7, 0)
     struct netlink_kernel_cfg cfg;
     cfg.groups = groups;
     cfg.input = input;
-    cfg.cb_mutex = pCb_mutex;
+    cfg.cb_mutex = cb_mutext;
     cfg.bind = NULL;
 
-    return netlink_kernel_create(pNet, unit, pModule, &cfg);
+    return netlink_kernel_create(net, unit, module, &cfg);
 #else
     struct netlink_kernel_cfg cfg;
     cfg.groups = groups;
     cfg.input = input;
-    cfg.cb_mutex = pCb_mutex;
+    cfg.cb_mutex = cb_mutext;
     cfg.bind = NULL;
 
-    return netlink_kernel_create(pNet, unit, &cfg);
+    return netlink_kernel_create(net, unit, &cfg);
 #endif
 }
 
-static int __init kernel_module_init(void)
+static int __init taco_initModule(void)
 {
-    g_pSocket = netlink_create_wrapper(&init_net, NETLINK_TEST, 0, process_user_msg, NULL, THIS_MODULE);
-    CHECK_IF(NULL == g_pSocket, return -1, "Module Insert Failed");
+    _socket = netlink_create_wrapper(&init_net, NETLINK_TEST, 0, process_user_msg, NULL, THIS_MODULE);
+    CHECK_IF(NULL == _socket, return -1, "Module Insert Failed");
 
     dprint("ok");
     return 0;
 }
 
-static void __exit kernel_module_exit(void)
+static void __exit taco_exitModule(void)
 {
-    netlink_kernel_release(g_pSocket);
-
+    netlink_kernel_release(_socket);
     dprint("ok");
     return;
 }
 
-module_init( kernel_module_init );
-module_exit( kernel_module_exit );
+module_init( taco_initModule );
+module_exit( taco_exitModule );
