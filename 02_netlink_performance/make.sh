01678372a6eaf35818bb5a466e0420fbc6544945
tacolin <potegrant@yahoo.com.tw>
2015-01-12 11:38:53 +0800

    加入編譯及測試的小腳本

---
+++ 02_netlink_performance/make.sh
@@ -0,0 +1,71 @@
+#!/usr/bin/env bash
+
+function DoMake()
+{
+    make -C $1
+}
+
+function DoClean()
+{
+    make clean -C $1
+}
+
+function ShowUsage()
+{
+    >&2 echo ""
+    >&2 echo "Usage: $0 "
+    >&2 echo -e "\t-u: make user_part"
+    >&2 echo -e "\t-k: make kernel_part"
+    >&2 echo -e "\t-c: clean user_part & kernel_part"
+    >&2 echo -e "\t-t: do test"
+    >&2 echo -e "\t-a: make all and do test"
+    >&2 echo ""
+    exit 1
+}
+
+function DoTest()
+{
+    cd kernel_part/
+    sudo insmod taco.ko
+
+    cd ../user_part/
+    ./main
+    ./main
+    ./main
+
+    sudo rmmod taco
+
+    dmesg
+}
+
+getopts "ukhcta" option
+case "${option}" in
+    u)
+        DoMake user_part
+        ;;
+
+    k)
+        DoMake kernel_part
+        ;;
+
+    c)
+        DoClean user_part
+        DoClean kernel_part
+        ;;
+
+    t)
+        DoTest
+        ;;
+
+    a)
+        DoClean user_part
+        DoMake user_part
+        DoClean kernel_part
+        DoMake kernel_part
+        DoTest
+        ;;
+
+    *|?)
+        ShowUsage $0
+        ;;
+esac
